e56389c73f63d91c92631df76acd1ca6c62680f9
Faouzi Mohamed <me@mfaouzi.com>
2026-03-11 09:12:46 +0000

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
+import {Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
 import {Providers} from '@/components/shared/providers';
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -24,7 +27,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="fr" suppressHydrationWarning>
+        <html lang="fr" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>{children}</Providers>
         </body>
